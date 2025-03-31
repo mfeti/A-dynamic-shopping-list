c10ebd13c6b1da4beb80246f9c19c2158ad25b84
mfeti <mfeti2175@gmail.com>
2025-03-31 07:10:26 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", addItem);
+ul.style.width = "50%";
+
+function addItem() {
+  const val = input.value;
+  input.value = "";
+
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const newBtn = document.createElement("button");
+  span.textContent = val;
+  newBtn.textContent = "Delete";
+
+  li.setAttribute("style", "display:flex;justify-content:space-between;");
+
+  li.appendChild(span);
+  li.appendChild(newBtn);
+
+  ul.appendChild(li);
+}
